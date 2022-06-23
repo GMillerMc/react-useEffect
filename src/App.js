5859944a8d9b005f05dc1c7bca7c1783de91756f
GMillerMc <gmiller04@yahoo.com>
2022-06-23 10:35:25 +0100

    push to github

--- src/App.js
+++ src/App.js
@@ -1,22 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
 
 function App() {
+  const [students, setStudents] = useState([]);
+  const [cohort, setCohort] = useState('');
+  const [search, setSearch] = useState('');
+  const [error, setError] = useState('');
+  const [statusMessage, setStatusMessage] = useState(true);
+
+  
+
+
+  useEffect(() => {
+    
+    const fetchStudents = async (searchTerm) => {
+      searchTerm ||= 'auguste'
+      try {
+
+    const url= `https://raw.githubusercontent.com/GMillerMc/fp_study_notes_hello_github/main/${searchTerm}/roster.json`
+
+    //const { data: { students } } = await axios.get(URL)
+    
+    const { data } = await axios.get(url)
+    console.log('I was Mounted')
+    setStudents(data.students)
+    setStatusMessage('')
+    setError('')
+    } catch (err) {
+      setError(err)
+      setStatusMessage('Loading...')
+  
+    }
+  }
+  const timeoutId = setTimeout(() => {
+    fetchStudents(search)
+  }, 400);
+
+  return () => {
+    clearTimeout(timeoutId)
+  }
+
+  }, [search])
+
+
+  console.log(cohort)
+  const renderedStudents = students.map(st => {
+    return (
+      <li key={st.github}>{st.github}</li>
+      ) //renders the students from the cohort - changed to github etc
+  })
+
+
+
+  const onInputChange = (e) => {
+    setCohort(e.target.value)
+  }
+
+  const onformSubmit = (e) => {
+    e.preventDefault()
+    setSearch(cohort)
+    setCohort('')
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {error
+          ? <h1>Sorry, we could not find a {search} cohort</h1>
+          : <div>
+            <h3> {statusMessage ? statusMessage : true } </h3>
+            <ul> {renderedStudents} </ul>
+          </div>
+        }
+      
+        <form onSubmit={onformSubmit}>
+          <label htmlFor="cohort"> Cohort
+          </label>
+          <input 
+          type="text"
+          id="cohort" 
+          value={cohort}
+          onChange={onInputChange}
+          />
+
+      
+        </form>
+    
       </header>
     </div>
   );
